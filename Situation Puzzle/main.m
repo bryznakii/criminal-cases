4bbae4feddd9569fea1cfc3806e230298dbc558f
jeffsun.tj <jeffsun.tj@gmail.com>
2011-12-07 14:39:44 +0000

    import initial folder

---
+++ Situation Puzzle/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Situation Puzzle
+//
+//  Created by Jeff on 11-12-7.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
