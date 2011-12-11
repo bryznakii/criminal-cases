1f98bab90ad87c450e3d3ae68db668a1d9290eb6
jeffsun.tj@gmail.com <jeffsun.tj@gmail.com@948ec394-8bd2-6c0c-60d3-2dc416d5b775>
2011-12-11 09:34:37 +0000

    add a very simple input processor...really very simple

---
+++ trunk/Situation Puzzle/SimpleNLProcessor.h
@@ -0,0 +1,15 @@
+//
+//  SimpleNLProcessor.h
+//  Situation Puzzle
+//
+//  Created by Jeff on 11-12-11.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleNLProcessor : NSObject
+
++(NSString *) getUserInput: (NSString *)input;
+
+@end
